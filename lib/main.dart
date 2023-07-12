65f01605d6d3ee3b25559802451dd3875b66da32
Ricardo Boss <mail@ricardoboss.de>
2023-07-12 19:29:05 +0200

    Added toggle for appBar, footerItems and an example for ScaffoldPage.scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,46 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _idx = 0;
+  bool _showAppBar = false;
 
   @override
   Widget build(BuildContext context) {
     return FluentApp(
       home: NavigationView(
-        appBar: const NavigationAppBar(
-          title: Text('Safe Area Bug'),
-        ),
+        appBar: _showAppBar
+            ? const NavigationAppBar(
+                title: Text('Safe Area Bug'),
+              )
+            : null,
         pane: NavigationPane(
+          selected: _idx,
+          onChanged: (i) => setState(() => _idx = i),
           items: [
             PaneItem(
               icon: const Icon(FluentIcons.home),
               title: const Text('Home'),
               body: ScaffoldPage.withPadding(
-                content: const Center(
-                  child: Text('Home'),
+                content: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('Show App Bar'),
+                      Checkbox(
+                        checked: _showAppBar,
+                        onChanged: (_) =>
+                            setState(() => _showAppBar = !_showAppBar),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -31,6 +53,21 @@ class MyApp extends StatelessWidget {
               body: ScaffoldPage.withPadding(
                 content: const Center(
                   child: Text('Settings'),
+                ),
+              ),
+            ),
+          ],
+          footerItems: [
+            PaneItem(
+              icon: const Icon(FluentIcons.sync),
+              title: const Text('Sync'),
+              body: ScaffoldPage.scrollable(
+                header: const PageHeader(
+                  title: Text('Example header'),
+                ),
+                children: List.generate(
+                  100,
+                  (index) => Text('Item $index'),
                 ),
               ),
             ),
